810d79b2bdb97aa803fe1ca534d71754f715e140
Juxhee <60679596+Juxhee@users.noreply.github.com>
2021-06-19 20:33:49 +0900

    Update oneclassSVM.py

--- oneclassSVM.py
+++ oneclassSVM.py
@@ -34,11 +34,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report 
 
 
-# ## train data (글로우픽 데이터)
-# - train data : 9500 (label : 1) 
-# - test data : 글로우픽 데이터 500 + 다른 플랫폼 데이터 약 1200개 (label : 1, -1) 
-
-# In[5]:
 
 
 data = pd.read_csv('./glowpick_review.csv',encoding='utf-8')
@@ -46,9 +41,6 @@ data = data.drop(["cmt_pk", "cmt_create_id",'time_update'], axis=1)
 data = data.drop(['idx','channel_type'], axis=1)
 data = data.drop(['time_create'],axis=1)
 
-#print(data)
-
-# data['cmt_like_count'].unique()
 
 df = data[:10000]
 #print(df)
@@ -67,18 +59,18 @@ df = df.drop(['data_pk'],axis=1)
 # random sampling
 df = df.sample(frac=1)
 
-# 최종 train data
+# train data
 train = df[:9500]
 # print(train)
 
-# test하는데 쓰일 글로우픽 데이터
+# glowpick test data
 test = df[9500:]
 
 
 #print(test['label'])
 
 
-# ## 다른 플랫폼 데이터 불러오기
+# ## other platform data (naver blog, youtube, powderroom, instagram)
 
 data1 = pd.read_csv('./review_11.csv',encoding='utf-8')
 data2 = pd.read_csv('./review_22.csv',encoding='utf-8')
@@ -92,35 +84,24 @@ data2 = data2[:300]
 data3 = data3[:150]
 data33 = data33.drop(['keyword'],axis=1)
 
-#train['label'].unique()
 
-# 다른 플랫폼 데이터 concat
+
+# concat
 new33 = pd.concat([data1,data2,data3,data33])
 
-#data33['label'].unique()
-#test['label'].unique()
-
 new33 = new33.reindex(columns=['cmt_body','label'])
-
-#new33
-#test['label'].unique()
-
-# 결측값 제거
 new33 = new33.dropna(axis=0)
-
-# label data type을 train data와 동일하게 변경
 new33 = new33.astype({'label': 'int64'})
 
-#new33['label'].unique()
 
-# 글로우픽 데이터와 다른 플랫폼 데이터 합친 test data생성
+
+# test data (glowpick + other platform)
 test = pd.concat([test,new33])
 
 # test data random sampling
 test = test.sample(frac=1)
 
 
-# 한글 빼고 모두 제거
 train['cmt_body']= train['cmt_body'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
 test['cmt_body']= test['cmt_body'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
 
@@ -133,32 +114,13 @@ train_labels = train['label'].tolist()
 test_text = test['cmt_body'].tolist()
 test_labels = test['label'].tolist()
 
-# 불용어 제거
+
 stopwords=['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다','넘']
 
-# 결측치 제거
 test = test.dropna(subset=["cmt_body"])
 
 
-# In[135]:
-
-
-#test_tokens = [tok for tok in v_text if len(tok) >= 3]
-
-
-# In[124]:
-
-
-# okt = Okt()
-# for sentence in test_text:
-#     temp_b = []
-#     temp_b = okt.morphs(sentence, stem=True) # 토큰화
-#     temp_b = [word for word in temp_b if not word in stopwords] # 불용어 제거
-#     v_text.append(temp_b)
-# test_tokens = [tok for tok in v_text]
-
-
-# ## token화
+# token
 def tokenText(sample) :
     okt = Okt()
     v_text = []
@@ -172,18 +134,15 @@ def tokenText(sample) :
 
 train_tokens = tokenText(train_text)
 test_tokens = tokenText(test_text)
-#print(train_text)
-#print(len(train_text))
-
 
 # ### HashingVectorizer
 #vectorizer = HashingVectorizer(n_features=300,tokenizer=tokenText)
 #features = vectorizer.fit_transform(train_text).toarray()
 
 
-# ## TfidfVectorizer
+# TfidfVectorizer
 
-# tfid 진행 위한 corpus 생성
+
 def corpus(tokens):
     corp = []
     for sent in tokens:
@@ -196,7 +155,7 @@ train_corp = corpus(train_tokens)
 #print(train_corp)
 test_corp = corpus(test_tokens)
 
-# ### tfidf train, test 데이터에 대해 함께
+
 def tfidfmatrix(corpus):
     vector = CountVectorizer(decode_error="replace")
     train_matrix = vector.fit_transform(train_corp)
@@ -217,14 +176,13 @@ test_matrix = tfidfmatrix(test_corp)
 
 
 # oneclassSVM
-# gridsearch로 parameter 정함
+# gridsearch
 
 param={'kernel':['linear', 'rbf', 'poly'],
       'gamma':[.1, .01, .001, .0001],
       'nu':[0.25, 0.5, 0.75, 0.95]}
 clf = GridSearchCV(estimator=OneClassSVM(),param_grid=param,cv=5, scoring = 'accuracy',n_jobs=-1)
 
-#len(train_labels)
 
 clf.fit(train_matrix,train_labels)
 
@@ -246,10 +204,3 @@ print(classification_report(test_labels, preds_test))
 roc = roc_auc_score(test_labels, preds_test)
 print(f'ROC score is {roc}')
 
-print(test_text[500])
-print('실제 label: {}'.format(test_labels[500]))
-print('예측 label: {}'.format(preds_test[500]))
-
-print(test_text[47])
-print('실제 label: {}'.format(test_labels[47]))
-print('예측 label: {}'.format(preds_test[47]))
